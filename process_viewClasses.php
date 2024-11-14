e567b0876ffd6efe90de828e16b51e07d3047695
Allan Fraker <fraker1832@uwlax.edu>
2024-11-13 21:23:29 -0600

    Update process_viewClasses.php

--- process_viewClasses.php
+++ process_viewClasses.php
@@ -1,59 +1,55 @@
 <?php
-
 session_start();
 include 'db_connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $Name = $_POST['Username'];
-
-    $userQuery = $pdo->prepare("SELECT UserID FROM Users WHERE Username = :Name");
-    $userQuery->execute(['Name' => $Name]);
+    $username = $_POST['Username'];
+    // Query for UserID based on Username
+    $userQuery = $pdo->prepare("SELECT UserID FROM Users WHERE Username = :username");
+    $userQuery->execute(['username' => $username]);
     $user = $userQuery->fetch(PDO::FETCH_ASSOC);
 
     if ($user) {
-        $classQuery = $pdo->prepare("SELECT Class FROM EnrollmentRecords WHERE User = :ID");
-        $classQuery->execute(['ID' => $user['UserID']]);
-        if($classQuery->rowCount() > 0) {
+        $classQuery = $pdo->prepare("SELECT Class FROM EnrollmentRecords WHERE User = :user_id");
+        $classQuery->execute(['user_id' => $user['UserID']]);
+
+        if ($classQuery->rowCount() > 0) {
             $classes = $classQuery->fetchAll(PDO::FETCH_ASSOC);
+            
             echo "<form action='action_ViewClasses.php' method='POST'>";
             echo "<table border='2'>";
             echo "<tr><th>Select</th><th>Class Name</th><th>Start Date</th><th>End Date</th><th>Days</th><th>Start Time</th><th>End Time</th><th>Location</th></tr>";
     
-            foreach($classes as $class) {
-                $classInfoQuery = $pdo->prepare("SELECT Name, StartDate, EndDate, Days, StartTime, EndTime, Location FROM Classes WHERE ClassID = :ClassID");
-                $classInfoQuery->execute(['ClassId' => $class]);
+            foreach ($classes as $class) {
+                $classInfoQuery = $pdo->prepare("SELECT Name, StartDate, EndDate, Days, StartTime, EndTime, Location FROM Classes WHERE ClassID = :class_id");
+                $classInfoQuery->execute(['class_id' => $class['Class']]);
                 $classInfo = $classInfoQuery->fetch(PDO::FETCH_ASSOC);
-                echo "<tr>";
-                echo "<td><input type='radio' name=classList id=" . $class . " value=" . $class . "></td>";
-                echo "<td>" . $classInfo['Name'] . "</td>";
-                echo "<td>" . $classInfo['StartDate'] . "</td>";
-                echo "<td>" . $classInfo['EndDate'] . "</td>";
-                echo "<td>" . $classInfo['Days'] . "</td>";
-                echo "<td>" . $classInfo['StartTime'] . "</td>";
-                echo "<td>" . $classInfo['EndTime'] . "</td>";
-                echo "<td>" . $classInfo['Location'] . "</td>";
-                echo "</tr>";
+                
+                if ($classInfo) {
+                    echo "<tr>";
+                    echo "<td><input type='radio' name='classList' id='" . htmlspecialchars($class['Class']) . "' value='" . htmlspecialchars($class['Class']) . "'></td>";
+                    echo "<td>" . htmlspecialchars($classInfo['Name']) . "</td>";
+                    echo "<td>" . htmlspecialchars($classInfo['StartDate']) . "</td>";
+                    echo "<td>" . htmlspecialchars($classInfo['EndDate']) . "</td>";
+                    echo "<td>" . htmlspecialchars($classInfo['Days']) . "</td>";
+                    echo "<td>" . htmlspecialchars($classInfo['StartTime']) . "</td>";
+                    echo "<td>" . htmlspecialchars($classInfo['EndTime']) . "</td>";
+                    echo "<td>" . htmlspecialchars($classInfo['Location']) . "</td>";
+                    echo "</tr>";
+                }
             }
             echo "</table>";
             echo "<input type='submit' value='Cancel'>";
             echo "</form>";
-    
+        } else {
+          
+            echo "<p>No classes found for the specified user.</p>";
         }
-        else {
-            header("Location: viewClasses.php?error=no_classes_found_for_user");
-            echo "<p> No classes found for the specified user. </p>";
-            exit();
-        }
+    } else {
+        echo "<p>Unable to find user.</p>";
     }
-    else {
-        header("Location: viewClasses.php?error=unable_to_find_user");
-        echo "<p>Unable to find user</p>";
-        exit();
-    }
-}
-else {
+} else {
     header("Location: viewClasses.php");
-    echo "<p> Unable to access class information at this time. </p>";
     exit();
 }
 ?>
